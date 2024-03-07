2d1acba36311ad9cc5a6dd06671c8ead92a345fb
Fernanda <152433962+MaFer74@users.noreply.github.com>
2024-03-07 01:16:41 +0000

    CHANGE MAP CODE

--- src/pages/projectGallery.jsx
+++ src/pages/projectGallery.jsx
@@ -1,16 +1,47 @@
 //components:
+//components:
 import React from "react"
 import projects from "./projectGallery.json"
 
 
 // const projects = require("/src/pages/projectGallery.json")
 
-function ProjectGallery(){
-   const projectListItems = projects.map(project=>
-   <li>
-    <div className="card">
-        <h5>{project.title}</h5>
-        </div>
-    </li>)
+function ProjectGallery() {
+    
+    return (
+        <section id="projectSection">
+        {projects.map(project => {
+           return <>
+           <li>
+                <div className="card">
+                    <h5>{project.title}</h5>
+                </div>
+            </li>
+           </>
+        })}
+        </section>
+    )
+
 }
 export default ProjectGallery
+
+
+
+
+
+
+// import React from "react"
+// import projects from "./projectGallery.json"
+
+
+// // const projects = require("/src/pages/projectGallery.json")
+
+// function ProjectGallery(){
+//    const projectListItems = projects.map(project=>
+//    <li>
+//     <div className="card">
+//         <h5>{project.title}</h5>
+//         </div>
+//     </li>)
+// }
+// export default ProjectGallery
